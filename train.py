25715e5e0aef914dcf51829b664bf234a432d520
zhangw <zhangw@uchoice.cc>
2023-09-16 22:30:17 +0800

    init config

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-model.train(data="config.yaml", epochs=25, save_period=5)  # train the model
+model.train(data="config.yaml", epochs=200, save_period=5)  # train the model
 
 # Keys to add
 # save_period:	Save checkpoint every x epochs (disabled if < 1)
